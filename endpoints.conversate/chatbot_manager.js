8afb6fd34fd448050162f99dfd1ba1d4fd350505
Matt Eckman <matteckman@gmail.com>
2016-10-20 19:54:01 -0400

    latest

--- endpoints.conversate/chatbot_manager.js
+++ endpoints.conversate/chatbot_manager.js
@@ -39,7 +39,7 @@ module.exports = function(BASEURL, PORT,db){
 			//removeOldBots();
 			console.log('sessionData',sessionData);
 			//sessionData.box
-			db.getBot(domain).then(function(botInfo){
+			db.getBot(sessionData.box.owner).then(function(botInfo){
 
 			if(!bots[room]) {
 					bots[room] = {};
